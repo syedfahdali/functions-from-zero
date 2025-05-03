38ee2a62964df98578def3f214f1c10c1b12e6bf
Syed Fahd Ali <100057960+syedfahdali@users.noreply.github.com>
2025-05-03 18:45:52 +0000

    fixed unused import

--- test_logistics.py
+++ test_logistics.py
@@ -1,7 +1,8 @@
-import pytest 
+import pytest
+
 # write tests for logisticsCLI
 from mylib.logistics import cities, distance_between_points, total_distance
 
-def distance_between_two_points():
-    assert distance_between_points(cities[0][1],cities[1][1]) == 2449.349999999995
 
+def distance_between_two_points():
+    assert distance_between_points(cities[0][1], cities[1][1]) == 2449.349999999995
